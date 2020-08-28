3da4708c6db794ea3e04e7f58cd3fd285d09c07f
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-08-28 18:44:49 -0300

    Configuracoes de Testes com Jest

--- packages/server/src/server.spec.ts
+++ packages/server/src/server.spec.ts
@@ -1,5 +1,5 @@
-describe("Test server", () => {
-  it("should do something", () => {
-    expect(1 + 4).toEqual(5);
-  });
-});
+describe('Test server', () => {
+  it('should do something', () => {
+    expect(1 + 4).toEqual(5)
+  })
+})
